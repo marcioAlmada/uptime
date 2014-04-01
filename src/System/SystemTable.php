02561c83daac8b0899397d967b611df7455a92e8
Márcio Almada <marcio3w@gmail.com>
2014-04-01 08:38:01 -0300

    add system / platform conversion table

---
+++ src/System/SystemTable.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Uptime\System;
+
+class SystemTable
+{
+    /**
+     * Map relating possible values of `PHP_OS` and widely known OS groups
+     * 
+     * Values should be declared in lowercase.
+     * 
+     * @link http://stackoverflow.com/questions/738823/possible-values-for-php-os
+     * @link http://en.wikipedia.org/wiki/Uname#Table_of_standard_uname_output
+     * @link https://github.com/php/php-src/blob/5b1f6caaf0574cd72396b1b4c671bae82557c4b4/configure.in
+     */
+    protected static $map = [
+        'Linux'   => [ 'linux', 'cygwin', 'linux-armv71', 'linux2', 'unix', 'sunos'], // `cat /proc/uptime`
+    ];
+
+    public static function getSystemIdentifier($system){
+        $system = strtolower($system);
+        foreach (self::getMap() as $identifier => $systems) {
+            if( in_array($system, $systems) ) return $identifier;
+        }
+        return false;
+    }
+
+    public static function getMap()
+    {
+        return self::$map;
+    }
+
+}
---
+++ test/System/SystemTableTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Uptime\System;
+
+/**
+ * SystemTableTest
+ *
+ * @group system
+ */
+class SystemTableTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider needleGroupProvider
+     */
+    public function testSystemTable($needle, $group)
+    {
+        $this->assertSame($group, (new SystemTable())->getSystemIdentifier($needle));
+    }
+
+    public function needleGroupProvider()
+    {
+        $randomcase = function(&$string) {
+            $chars = str_split($string);
+            array_walk($chars, function( &$char ) {
+                rand(0, 1) ? $char = strtoupper($char) : $char = strtolower($char);
+            });
+            return implode('', $chars);
+        };
+        $data = [];
+        foreach  ( SystemTable::getMap() as $group => $systems ) {
+            $data[] = [$group, $group];
+            foreach( $systems as $system ) {
+                $data[] = [ucfirst($system), $group];
+                $data[] = [$randomcase($system), $group];
+            }
+        }
+        return $data;
+    }
+}
