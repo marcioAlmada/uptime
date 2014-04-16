52c1a6747b3aec3567279485097d510a29233c65
Márcio Almada <marcio3w@gmail.com>
2014-04-16 15:45:01 -0300

    add test for system + runtime integration

--- test/SystemTest.php
+++ test/SystemTest.php
@@ -27,4 +27,26 @@ class SystemTest extends \PHPUnit_Framework_TestCase
         ];
     }
 
+    /**
+     * @test
+     * @dataProvider systemIdentifierProvider
+     */
+    public function tetsCurrentSystem( $system_identifier )
+    {
+        if($system_identifier !== PHP_OS) {
+            $this->markTestSkipped("Skipped {$system_identifier} related test.");
+        }
+        $system = new System($system_identifier);
+        $this->assertInstanceOf('\Uptime\Uptime', $system->getUptime());
+        $this->assertInstanceOf('\Uptime\Boottime', $system->getBoottime());
+        $this->assertGreaterThan(0, $system->getUptime()->getRaw());
+        $this->assertGreaterThan(0, $system->getBoottime()->getRaw());
+    }
+
+    public function systemIdentifierProvider()
+    {   
+        $systems = array_keys(SystemTable::getMap());
+        array_walk($systems, function(&$group){ $group =  [$group]; });
+        return $systems;
+    }
 }
