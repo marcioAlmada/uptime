417180cca474d28949c7f96e2739c8b75e4e2877
Jon Haynes <jon.haynes@icnetplc.com>
2016-05-03 12:28:34 +0100

    Updated unit test for windows boottime

--- test/Runtime/Windows/BoottimeTest.php
+++ test/Runtime/Windows/BoottimeTest.php
@@ -22,14 +22,14 @@ class BoottimeTest extends \PHPUnit_Framework_TestCase
 
     public function stdinProvider()
     {
-        $factory = function ($timestamp, $stdout) {
-            return [ $timestamp, sprintf($stdout, $timestamp) ];
+        $factory = function ($timestamp, $inputString, $stdout) {
+            return [ $timestamp, sprintf($stdout, $inputString) ];
         };
 
         return [
-            $factory('20140429225056.153625-180', "LastBootUpTime\n%s\n\n\n"),
-            $factory('20120612125056.153625-060', "LastBootUpTime\n%s\n\n\n"),
-            $factory('20130921162204.153625+120', "LastBootUpTime\n%s\n\n\n"),
+            $factory(1398820256, '20140429225056.153625-180', "LastBootUpTime\n%s\n\n\n"),
+            $factory(1339509056, '20120612125056.153625-060', "LastBootUpTime\n%s\n\n\n"),
+            $factory(1379775724, '20130921162204.153625+120', "LastBootUpTime\n%s\n\n\n"),
         ];
     }
 }
